affd38e231404aa33cec93e1b215cd5fe50ff149
przybylski46 <przybylski4637@gmail.com>
2026-04-30 12:34:05 -0600

    Add data.js with user profile information

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+  displayName: "Pino",
+  username: "pino",
+
+  avatar: "https://i.imgur.com/your-pine.png",
+
+  friends: 0,
+  followers: "999K",
+  following: 1,
+
+  bio: "🌲 Solo soy un pino tranquilo en Roblox",
+
+  items: [
+    {
+      name: "Hojas verdes",
+      img: "https://i.imgur.com/your-pine.png"
+    },
+    {
+      name: "Tronco",
+      img: "https://i.imgur.com/your-pine.png"
+    }
+  ]
+};
