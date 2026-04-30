d43d9114fc58c1e8677e1f2e692876e3f0fa8c61
przybylski46 <przybylski4637@gmail.com>
2026-04-30 12:41:41 -0600

    Actualizar data.js

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 const data = {
   displayName: "Pino",
-  username: "pino",
+  username: "PINO_69",
 
-  avatar: "https://i.imgur.com/your-pine.png",
+  avatar: "https://cdn.discordapp.com/attachments/1498040372323024906/1499449430330445944/thumb-spruce3.png?ex=69f4d69a&is=69f3851a&hm=67fc8a0b1e1555269c3540df5e0cbf38f2e81398d8ed87f429b941bf7a0f370a&",
 
   friends: 0,
-  followers: "999K",
+  followers: "69K",
   following: 1,
 
-  bio: "🌲 Solo soy un pino tranquilo en Roblox",
+  bio: "🌲 1 - 0 🐒",
 
   items: [
     {
-      name: "Hojas verdes",
-      img: "https://i.imgur.com/your-pine.png"
+      name: "Hojas",
+      img: "https://cdn.discordapp.com/attachments/1498040372323024906/1499480522865246289/Spruce_Leaves.webp?ex=69f4f38f&is=69f3a20f&hm=6c0d945cc9c5209b3ee3bcf718db677af90c2555c0fa1e854df662b350bd6624&"
     },
     {
       name: "Tronco",
-      img: "https://i.imgur.com/your-pine.png"
+      img: "https://cdn.discordapp.com/attachments/1498040372323024906/1499480513805811943/Spruce_Log_28UD29_JE4.webp?ex=69f4f38d&is=69f3a20d&hm=34cc16fa539bb19ed4ad0de33a4a620f1ae1c61e8fb113711b6aa79a0d1b0181&"
     }
   ]
 };
